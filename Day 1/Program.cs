44d8e30fcf0b1e9f8c55d8e07bbf16d1ddb2b8fc
HeathAndrew <andrew.heath@magstim.com>
2022-12-01 11:51:56 +0000

    Init

---
+++ Day 1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = System.IO.File.ReadAllLines(@".\input.txt");
+            List<int> elvesTotal = new List<int>();
+
+            for(int i =0; i < lines.Length; i++){
+                while(lines[i] != ""){
+                    
+                }
+                Console.WriteLine(lines[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
