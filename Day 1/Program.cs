bf0b434fa8c114dc5869c26f391736191ce48e55
HeathAndrew <andrew.heath@magstim.com>
2022-12-01 12:33:56 +0000

    Day 1 Complete

--- Day 1/Program.cs
+++ Day 1/Program.cs
@@ -6,13 +6,23 @@
         {
             string[] lines = System.IO.File.ReadAllLines(@".\input.txt");
             List<int> elvesTotal = new List<int>();
-
+            int total = 0;
             for(int i =0; i < lines.Length; i++){
-                while(lines[i] != ""){
-                    
-                }
-                Console.WriteLine(lines[i]);
+               if(lines[i] == "" || lines[i] == string.Empty)
+               {
+                elvesTotal.Add(total);
+                total = 0;
+               }else{
+                    total += int.Parse(lines[i]);
+               } 
             }
+            int sum = elvesTotal.Max();
+            elvesTotal.Remove(elvesTotal.Max());
+            sum += elvesTotal.Max();
+            elvesTotal.Remove(elvesTotal.Max());
+            sum += elvesTotal.Max();
+            
+            Console.WriteLine(String.Format("Highest Value: {0} \nSum of Top 3: {1}", elvesTotal.Max(),sum));
             Console.ReadLine();
         }
     }
