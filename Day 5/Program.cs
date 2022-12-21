b7c8500b66d162683f52c0a584e901873e3cc603
HeathAndrew <andrew.heath@magstim.com>
2022-12-21 16:32:09 +0000

    init

---
+++ Day 5/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string lines = System.IO.File.ReadAllText(@".\input.txt");
+
+            string[] parts = lines.Split("\n\r");
+
+            Console.WriteLine(parts[0]);
+            Console.WriteLine("------------------------------------------------------------------------");
+            Console.WriteLine(parts[1]);
+        }
+    }
+}
