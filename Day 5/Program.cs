80ed60de18a1d15e7efb95c5aaec855c36d8449f
HeathAndrew <andrew.heath@magstim.com>
2022-12-21 16:45:14 +0000

    splits stacks into seperate lines

--- Day 5/Program.cs
+++ Day 5/Program.cs
@@ -8,9 +8,16 @@
 
             string[] parts = lines.Split("\n\r");
 
+            List<string> col1,col2,col3,col4,col5,col6,col7,col8,col9;
+
             Console.WriteLine(parts[0]);
             Console.WriteLine("------------------------------------------------------------------------");
             Console.WriteLine(parts[1]);
+
+            string[] part1lines = parts[0].Split("\r\n");
+
+
+
         }
     }
 }
